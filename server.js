7846734b66fd21d1c46fbf260db393308a587fa6
ctsygiel <ctsygiel@gmail.com>
2017-04-17 14:23:02 -1000

    All mehtods are working, now fixing number counter bug

--- server.js
+++ server.js
@@ -53,19 +53,15 @@ const server = http.createServer((req, res) => {
 <p><a href="/">back</a></p>
 </body>
 </html>`);
-      ++number;
-      console.log('number',number);
 
-      let counter = `<h3>There are ${number}</h3>`;
       let indexRead = fs.readFileSync('./public/index.html', 'utf-8');
-      console.log(indexRead);
       let elementHTML = `<li>
 <a href="/${element}.html">${element}</a>
 </li>
 <!-- new  -->`;
+      ++number;
       let elementInsert = indexRead.replace(/(<!-- new  -->)/g, elementHTML);
       elementInsert = elementInsert.replace(/(<h3>There are [^A-Z])/g, `<h3>There are ${number}`);
-      console.log(elementInsert);
 
       fs.writeFile('./public/index.html', elementInsert, 'utf-8', (err) => {
           console.log('new element added!');
@@ -75,10 +71,82 @@ const server = http.createServer((req, res) => {
 
   });
 
+  }
+
+  //PUT method
+  if(req.method === 'PUT'){
+    req.on('data', (data) => {
+      let elementObj = querystring.parse(data.toString());
+      element = elementObj.elementName;
+      elementSymbol = elementObj.elementSymbol;
+      elementAtomic = elementObj.elementAtomicNumber;
+      elementDescription = elementObj.elementDescription;
+
+      let newElement = `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>The Elements - ${element}</title>
+<link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+<h1>${element}</h1>
+<h2>${elementSymbol}</h2>
+<h3>Atomic number ${elementAtomic}</h3>
+<p>${elementDescription}</p>
+<p><a href="/">back</a></p>
+</body>
+</html>`;
+
+      let elementRead = fs.readFileSync(`./public${req.url}`, 'utf-8');
+
+      fs.writeFile(`./public${req.url}`, newElement, 'utf-8', (err) => {
+          console.log('modified');
+      });
+
+    res.end(data);
+    });
+  }
 
 
+  //DELETE method
+  if(req.method === 'DELETE'){
+
+    req.on('data', (data) => {
+      // remove element HTML file
+      // fs.unlink(`./public${req.url}`, (err) => {
+      //   console.log('deleted');
+      // });
+      let str = req.url;
+      str = str.slice(1,-5);
+
+
+      //edit index HTML file to remove link and decrease number by 1.
+      let indexRead = fs.readFileSync('./public/index.html', 'utf-8');
+      indexRead = indexRead.split('\n');
+      console.log(`<a href="${req.url}">${str.charAt(0).toUpperCase() + str.slice(1)}</a>`);
+      console.log(indexRead.indexOf(`      <a href="${req.url}">${str.charAt(0).toUpperCase() + str.slice(1)}</a>`));
+      let updateIndex = indexRead.splice(indexRead.indexOf(`      <a href="${req.url}">${str.charAt(0).toUpperCase() + str.slice(1)}</a>`) -1, 1);
+      updateIndex = indexRead.splice(indexRead.indexOf(`      <a href="${req.url}">${str.charAt(0).toUpperCase() + str.slice(1)}</a>`) + 1, 1);
+      updateIndex = indexRead.splice(indexRead.indexOf(`      <a href="${req.url}">${str.charAt(0).toUpperCase() + str.slice(1)}</a>`) , 1);
+
+
+      indexRead = indexRead.join('\n').replace(/(<h3>There are [^A-Z])/g, `<h3>There are ${number}`);
+      console.log('updated', indexRead);
+
+
+
+
+      // fs.writeFile('./public/index.html', indexRead, 'utf-8', (err) => {
+      //     console.log('modified index!');
+      // });
+
+
+    res.end(data);
+    });
 
   }
+
 
 
 });
